9c634ee710e0672359f10c8caa16283781010949
Vish098 <vishnupriyakp1996@gmail.com>
2026-04-22 15:59:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let totalHours = 0;
 
+function addTask() {
+    let subject = document.getElementById("subject").value;
+    let hours = Number(document.getElementById("hours").value);
+
+    if (subject === "" || hours <= 0) {
+        alert("Enter valid data");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerText = subject + " - " + hours + " hrs";
+
+    document.getElementById("list").appendChild(li);
+
+    totalHours += hours;
+
+    document.getElementById("total").innerText =
+        "Total Study Hours: " + totalHours;
+
+    document.getElementById("subject").value = "";
+    document.getElementById("hours").value = "";
+}
+
+function clearAll() {
+    document.getElementById("list").innerHTML = "";
+    totalHours = 0;
+    document.getElementById("total").innerText = "";
+}
